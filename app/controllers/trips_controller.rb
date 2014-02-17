1236c85a3d40d7318d419d491d54e2f2cd48be06
Andy Richardson <axrich@gmail.com>
2014-02-17 00:19:29 -0600

    current (local) time

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
   
   def index
     # @trip = Trip.first
-    @current_hour =  Time.new.localtime.strftime("%H")
+    @current_hour =  Time.zone.now.hour
   end
 
 end
